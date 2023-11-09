01f45cf1a60c666831f7885f627b92aa3bb0a451
Daniel Foister <foisterdaniel@gmail.com>
2023-11-09 11:43:19 -0500

    added the header to the master program.cs

--- CSCImiamiWarehouseSimulation/Program.cs
+++ CSCImiamiWarehouseSimulation/Program.cs
@@ -1,4 +1,13 @@
-﻿namespace CSCImiamiWarehouseSimulation
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+// Author: Daniel Foister, Curtis Reece, Sterling McClanahan, Chris Oaks
+// Course: CSCI-2210-001 - Data Structures
+// Assignment: Project3 - Warehouse Simulation
+// Description: A demonstration of our understanding and proficiency in the data structures we have discussed in class,
+//              in the form of a warehouse simulation.
+//
+///////////////////////////////////////////////////////////////////////////////
+namespace CSCImiamiWarehouseSimulation
 {
     internal class Program
     {
