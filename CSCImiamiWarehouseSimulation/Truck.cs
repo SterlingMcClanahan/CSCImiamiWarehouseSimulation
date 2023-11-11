cae5092159cfb17c4f43ec3eaad369f33a8567ed
Curtis Reece <REESECL@ETSU.EDU>
2023-11-10 21:03:23 -0500

    Added functionality to the run method and updated the truck class to supplement said functionality.

--- CSCImiamiWarehouseSimulation/Truck.cs
+++ CSCImiamiWarehouseSimulation/Truck.cs
@@ -22,11 +22,55 @@ namespace CSCImiamiWarehouseSimulation
             to back
         */
 
+        /// <summary>
+        /// The name of the truck driver associated with the truck.
+        /// </summary>
         public string driver { get; private set; }
+
+        /// <summary>
+        /// The name of the delivery company associated with the truck.
+        /// </summary>
         public string deliveryCompany { get; private set; }
 
+        /// <summary>
+        /// The stack of crates inside of the truck.
+        /// </summary>
         public Stack<Crate> Trailer;
 
+        /// <summary>
+        /// A list of potential driver names from the 1994 Japanese Baseball game "Fighting Baseball".
+        /// </summary>
+        public static List<string> driverNames = new List<string>
+            {
+                "Sleve McDichael", "Onson Sweemey", "Darryl Archideld",
+                "Anatoli Smorin", "Rey McSriff" ,"Glenallen Mixon",
+                "Mario McRlwain", "Raul Chamgerlain", "Kevin Nogilny",
+                "Tony Smehrik", "Bobson Dugnutt", "Willie Dustice",
+                "Jeromy Gride", "Scott Dourque", "Shown Furcotte",
+                "Dean Wesrey", "Mike Truk", "Dwigt Rortugal",
+                "Tim Sandaele", "Karl Dandleton", "Mike Sernandez",
+                "Todd Bonzalez"
+            };
+
+        /// <summary>
+        /// A list of potential delivery company names. (Copied from Github).
+        /// </summary>
+        public static List<string> deliveryCompanies = new List<string>
+            {
+                "Acme Corporation", "Globex Corporation", "Soylent Corp",
+                "Initech", "Bluth Company", "Umbrealla Corporation",
+                "Hooli", "Duff Beer", "Massive Dynamic",
+                "Wonka Industries", "Stark Industries", "Gekko & Co",
+                "Wayne Enterprises", "Bubba Gump", "Cyberdyne Systems",
+                "Genco Pura Olive Oil Company", "The New York Inquirer"
+            };
+
+        public static int minimumCrateNumber = 3;
+        public static int maximumCrateNumber = 8;
+
+        /// <summary>
+        /// Default constructor for the truck.
+        /// </summary>
         public Truck()
         {
             driver = string.Empty;
@@ -34,6 +78,11 @@ namespace CSCImiamiWarehouseSimulation
             Stack<Crate> Trailer = new Stack<Crate>();
         }
 
+        /// <summary>
+        /// Parameterized constructor for the truck.
+        /// </summary>
+        /// <param name="driver">The name of the truck driver associated with the truck.</param>
+        /// <param name="deliveryCompany">The name of the delivery company associated with the truck.</param>
         public Truck(string driver, string deliveryCompany)
         {
             this.driver = driver;
@@ -41,13 +90,41 @@ namespace CSCImiamiWarehouseSimulation
             Stack<Crate> Trailer = new Stack<Crate>();
         }
 
-        //might have to do a special case for first item (not sure)
+        /// <summary>
+        /// Generates a truck using the parameterized constructor and a random driver name and delivery company from a preset list of names.
+        /// </summary>
+        /// <returns></returns>
+        public static Truck GenerateTruck()
+        {
+            Random random = new Random();
+
+            //Generate random driver and company names and use the parameterized constructor.
+            Truck truck = new Truck(driverNames[random.Next(0, Truck.driverNames.Count - 1)], deliveryCompanies[random.Next(0, Truck.deliveryCompanies.Count - 1)]);
+
+            //Add a random amount of crates to the truck.
+            for (int i = 0; i < new Random().Next(minimumCrateNumber, maximumCrateNumber); i++)
+            {
+                Crate crate = new Crate();
+                truck.Load(crate);
+            }
+
+            return truck;
+        }
+
+        /// <summary>
+        /// A method that loads crates into the trailer by pushing them onto a stack.
+        /// </summary>
+        /// <param name="crate">The crate that was pushed onto the stack.</param>
         public void Load(Crate crate)
         {
             Trailer.Push(crate);
         }
 
-        //might have to do a special case to check if there is any item to remove (not sure)
+        /// <summary>
+        /// A method that removes crates from the trailer by popping them from the stack.
+        /// </summary>
+        /// <returns>The crate the was popped from the stack.</returns>
+        /// <exception cref="IndexOutOfRangeException">An exception thrown when trying to remove crates from an empty stack.</exception>
         public Crate Unload()
         {
             if (HasMoreCrates())
@@ -61,6 +138,10 @@ namespace CSCImiamiWarehouseSimulation
             
         }
 
+        /// <summary>
+        /// A method that determines if the truck has more crates in its trailer.
+        /// </summary>
+        /// <returns>A boolean value of true if there are more crates and false if there are not any more crates.</returns>
         public bool HasMoreCrates()
         {
             if (Trailer.Count > 0)
--- CSCImiamiWarehouseSimulation/Warehouse.cs
+++ CSCImiamiWarehouseSimulation/Warehouse.cs
@@ -7,6 +7,8 @@
 //              in the form of a warehouse simulation.
 //
 ///////////////////////////////////////////////////////////////////////////////
+using System.Security.Cryptography.X509Certificates;
+
 namespace CSCImiamiWarehouseSimulation
 {
     internal class Warehouse
@@ -15,9 +17,140 @@ namespace CSCImiamiWarehouseSimulation
         List<Dock> docks = new List<Dock>();
         Queue<Truck> entrance = new Queue<Truck>();
 
+        public Warehouse()
+        {
+            docks.Clear();
+            entrance.Clear();
+        }
+
         public static void Run()
         {
-            //run the simulation
+            //Initial Setup of Simulation and its parameters.
+            int timeIncrements = 48; //Each increment represents 10 minutes out of an 8 hour shift.
+            int currentTime = 0;
+            int numberOfDocks = 10;
+            int numberOfTrucks = 0;
+            float chanceOfGeneratingTruck = 0;
+            int maxPossibleTrucksPerTimeIncrement = 5;
+
+            //Setup of Warehouse
+            Warehouse warehouse = new Warehouse();
+
+
+            //Setup of Docks
+            for (int i = 0; i < numberOfDocks; i++)
+            {
+                Dock dock = new Dock();
+                warehouse.docks.Add(dock);
+            }
+
+            //Setup of Trucks
+            List<Truck>[] trucks = new List<Truck>[timeIncrements];
+
+            //For the number of Time Increments.
+            for(int i = 0; i < timeIncrements; i++)
+            {
+                //Determine how many trucks per time increment.
+
+                //This is where we need to do the normal distribution code.
+                if(i < timeIncrements / 2)
+                {
+                    chanceOfGeneratingTruck = i / timeIncrements;
+                }
+                else
+                {
+                    chanceOfGeneratingTruck = (timeIncrements - i) / (timeIncrements / 2);
+                }
+
+                //Attempt to make a truck a number of times equal to the max number of trucks possible per time increment.
+                for(int j = 0; j < maxPossibleTrucksPerTimeIncrement; j++)
+                {
+                    if (new Random().NextDouble() >= chanceOfGeneratingTruck)
+                    {
+                        //Generate a truck.
+                        Truck truck = Truck.GenerateTruck();
+                        trucks[i].Add(truck);
+                    }
+                }
+            }
+
+            //For loop that runs the actual simulation and updates every time increment.
+            for (currentTime = 0; currentTime < timeIncrements; currentTime++)
+            {
+                //Truck Arrivals at the Entrance
+                foreach (Truck truck in trucks[currentTime])
+                {
+                    warehouse.entrance.Enqueue(truck);
+                }
+
+                //Trucks Assigned to Docks
+                foreach (Truck truck in warehouse.entrance)
+                {
+                    int indexOfDockWithSmallestLine = 0;
+                    //Loop through each dock to find the one with the smallest line.
+                    for (int j = 1; j < warehouse.docks.Count; j++)
+                    {
+                        if (warehouse.docks[j].Line.Count < warehouse.docks[indexOfDockWithSmallestLine].Line.Count)
+                        {
+                            indexOfDockWithSmallestLine = j;
+                        }
+                    }
+
+                    //Add the truck to the Dock
+                    warehouse.docks[indexOfDockWithSmallestLine].JoinLine(truck);
+                    //Note: Trucks can be added to a dock every time increment, but it doesn't say whether multiple trucks
+                    //can be added to the same dock or not. This is assuming that they can in cases of small numbers of docks and a lot of trucks.
+                }
+
+                //Process the docks by unloading a crate.
+                //Handle the scenario of swapping to the next queued truck if last crate was unloaded.
+                foreach (Dock dock in warehouse.docks)
+                {
+                    Truck currentTruck;
+                    if (dock.Line.Count > 0)
+                    {
+                        currentTruck = dock.Line.Peek();
+                        if (currentTruck.HasMoreCrates())
+                        {
+                            Crate currentCrate = currentTruck.Unload();
+                            dock.TotalCrates++;
+                            dock.TotalSales += currentCrate.Price;
+                        }
+
+                        if (currentTruck.HasMoreCrates())
+                        {
+                            //Situation where crate has been unloaded and there are more crates to unload.
+                            //Do nothing currently, but eventually add logging info here and nothing else.
+                        }
+                        else
+                        {
+                            //Situation where crate has been unloaded and the truck has no more crates to unload.
+                            dock.SendOff();
+                            dock.TotalTrucks++;
+                            
+                            if (dock.Line.Count > 0)
+                            {
+                                //And another truck is already in the Dock
+                                //Do nothing currently, but eventually add logging info here and nothing else.
+                            }
+                            else if (dock.Line.Count = 0)
+                            {
+                                //But another truck is NOT already in the Dock
+                                //Do nothing currently, but eventually add logging info here and nothing else.
+                            }
+                        }
+
+                        dock.TimeInUse++;
+                    }
+                    else
+                    {
+                        dock.TimeNotInUse++;
+                    }
+                }
+
+
+            }
+
         }
 
     }
